d9a9b254c84be099b146693cb59721e63f35021e
Imaduddin Haetami <deen24id@gmail.com>
2025-05-11 05:12:32 +0700

    Remove dead code.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
 
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { Form } from "@/components/form";
 
 const p = 60 * 60 * 1000;
